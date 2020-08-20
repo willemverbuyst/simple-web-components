7df6c0587d2ef3c06ec6fcdc6fa7fa04033cc993
Willem Verbuyst <willemverbuyst@gmail.com>
2020-08-20 19:47:50 +0200

    Solution ex. 1

---
+++ toggle-button.js
@@ -0,0 +1,39 @@
+class ToggleContainer extends HTMLElement {
+  constructor() {
+    super();
+    this._isVisible = false;
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.innerHTML = `
+      <style>
+        #info-box {
+          display: none;
+        }
+      </style>
+      <button>Show</button>
+      <p id='info-box'>
+        <slot></slot>
+      <p>
+      `;
+    this._infoEl = this.shadowRoot.querySelector('p');
+    this._button = this.shadowRoot.querySelector('button');
+    this._button.addEventListener('click', this._toggleText.bind(this));
+  }
+
+  connectedCallback() {
+    if (this.hasAttribute('is-visible')) {
+      if (this.getAttribute('is-visible') === 'true') {
+        this._isVisible = true;
+        this._infoEl.style.display = 'block';
+        this._button.textContent = 'Hide';
+      }
+    }
+  }
+
+  _toggleText() {
+    this._isVisible = !this._isVisible;
+    this._infoEl.style.display = this._isVisible ? 'block' : 'none';
+    this._button.textContent = this._isVisible ? 'Hide' : 'Show';
+  }
+}
+
+customElements.define('uc-toggle-container', ToggleContainer);
