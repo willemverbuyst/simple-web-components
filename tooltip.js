10f3e39176f4e1ecdbe1f577f35fa3afab33b6bd
Willem Verbuyst <willemverbuyst@gmail.com>
2020-08-20 16:38:38 +0200

    Use attributes

--- tooltip.js
+++ tooltip.js
@@ -2,9 +2,14 @@ class Tooltip extends HTMLElement {
   constructor() {
     super();
     this._tooltipContainer;
+    this._tooltipText = 'Some dummy tooltip text';
   }
 
   connectedCallback() {
+    if (this.hasAttribute('text')) {
+      this._tooltipText = this.getAttribute('text');
+    }
+
     const tooltipIcon = document.createElement('span');
     tooltipIcon.textContent = ' (?) ';
     tooltipIcon.addEventListener('mouseenter', this._showToolTip.bind(this));
@@ -14,7 +19,7 @@ class Tooltip extends HTMLElement {
   // _ indicates private property
   _showToolTip() {
     this._tooltipContainer = document.createElement('div');
-    this._tooltipContainer.textContent = 'this is the tooltip text';
+    this._tooltipContainer.textContent = this._tooltipText;
     this.appendChild(this._tooltipContainer);
   }
 
