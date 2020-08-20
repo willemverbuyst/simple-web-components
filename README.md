adafe9d3b50ffb1a0ddfe81ca43afc6db9756153
Willem Verbuyst <willemverbuyst@gmail.com>
2020-08-20 16:29:37 +0200

    Listen to events

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## What is this?
 
-A repo for 'A Complete Introduction to building Custom HTML Elements/ Web Components with and without StencilJS' by Academind by Maximilian Schwarzmüller.
+A repo for 'A Complete Introduction to building Custom HTML Elements/ Web Components with and without StencilJS' by Maximilian Schwarzmüller.
 
 Contains code-alongs and exercises.
--- tooltip.js
+++ tooltip.js
@@ -1,12 +1,25 @@
 class Tooltip extends HTMLElement {
   constructor() {
     super();
+    this._tooltipContainer;
   }
 
   connectedCallback() {
     const tooltipIcon = document.createElement('span');
     tooltipIcon.textContent = ' (?) ';
+    tooltipIcon.addEventListener('mouseenter', this._showToolTip.bind(this));
+    tooltipIcon.addEventListener('mouseleave', this._hideToolTip.bind(this));
     this.appendChild(tooltipIcon);
+  }
+  // _ indicates private property
+  _showToolTip() {
+    this._tooltipContainer = document.createElement('div');
+    this._tooltipContainer.textContent = 'this is the tooltip text';
+    this.appendChild(this._tooltipContainer);
+  }
+
+  _hideToolTip() {
+    this.removeChild(this._tooltipContainer);
   }
 }
 
