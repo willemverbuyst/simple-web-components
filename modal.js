96b404c7be6e499240c2b66010e4525f14f028b0
Willem Verbuyst <willemverbuyst@gmail.com>
2020-08-22 07:03:24 +0200

    Add method to open modal

--- modal.js
+++ modal.js
@@ -1,6 +1,7 @@
 class Modal extends HTMLElement {
   constructor() {
     super();
+    this.isOpen = false;
     this.attachShadow({ mode: 'open' });
     this.shadowRoot.innerHTML = `
       <style>
@@ -78,20 +79,22 @@ class Modal extends HTMLElement {
     `;
   }
 
-  // attributeChangedCallback(name, oldValue, newValue) {
-  //   if (name === 'opened') {
-  //     if (this.hasAttribute('opened')) {
-  //       this.shadowRoot.querySelector('#backdrop').style.opacity = 1;
-  //       this.shadowRoot.querySelector('#backdrop').style.pointerEvents = 'all';
-  //       this.shadowRoot.querySelector('#modal').style.opacity = 1;
-  //       this.shadowRoot.querySelector('#modal').style.pointerEvents = 'all';
-  //     }
-  //   }
-  // }
+  atrributeConnectedCallback(name, oldvalue, newValue) {
+    if (this.hasAttribute('opened')) {
+      this.isOpen = true;
+    } else {
+      this.isOpen = false;
+    }
+  }
 
-  // static get observedAttributes() {
-  //   return ['opened'];
-  // }
+  static get observedAttributes() {
+    return ['opened'];
+  }
+
+  open() {
+    this.setAttribute('opened', '');
+    this.isOpen = true;
+  }
 }
 
 customElements.define('uc-modal', Modal);
